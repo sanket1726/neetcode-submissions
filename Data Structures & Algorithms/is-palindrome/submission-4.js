28c0b0116e0f00252b4092227af44632dc166a81
Sanket Benade <er.sanketbenade@gmail.com>
2026-09-25 23:41:53 +0530

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+
+    isPalindrome(s) {
+        let left = 0;
+        let right = s.length - 1;
+
+        function isAlphaNumeric(s) {
+            return /[a-zA-Z0-9]/.test(s);
+        }
+
+        while (left < right) {
+
+            // Skip non-alphanumeric characters
+            if (left < right && !isAlphaNumeric(s[left])) {
+                left++;
+                continue;
+            }
+
+            if (left < right && !isAlphaNumeric(s[right])) {
+                right--;
+                continue;
+            }
+
+            // Compare characters
+            if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
