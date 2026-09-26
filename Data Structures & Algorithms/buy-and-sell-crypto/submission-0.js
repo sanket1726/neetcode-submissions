6c3a38798c6233c1f03b483baf106e8e51b920eb
Sanket Benade <er.sanketbenade@gmail.com>
2026-09-26 23:46:19 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+       let left = 0;
+    let right = 1;
+    let maxProfit = 0;
+
+    while (right < prices.length) {
+        if (prices[right] > prices[left]) {
+            const profit = prices[right] - prices[left];
+
+            maxProfit = Math.max(maxProfit, profit);
+        } else {
+            // Found a cheaper buying price
+            left = right;
+        }
+
+        right++;
+    }
+
+    return maxProfit;
+    }
+}
